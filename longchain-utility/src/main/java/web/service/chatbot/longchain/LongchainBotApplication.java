6ba305b5aba6e0772635346b87d085faba322875
stevenMarmion <marmionsteven07@gmail.com>
2025-04-29 22:47:53 +0200

    Ajout base de code intitial

---
+++ longchain-utility/src/main/java/web/service/chatbot/longchain/LongchainBotApplication.java
@@ -0,0 +1,13 @@
+package web.service.chatbot.longchain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LongchainBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LongchainBotApplication.class, args);
+	}
+
+}
---
+++ longchain-utility/src/test/java/web/service/chatbot/longchain/LongchainApplicationTests.java
@@ -0,0 +1,13 @@
+package web.service.chatbot.longchain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LongchainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
